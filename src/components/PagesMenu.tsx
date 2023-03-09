bc5b03473bc31fb4cb15503fc3b44b4701f95023
Dodô <evandro.carreira@gmail.com>
2023-03-09 18:43:00 -0300

    fix(logo): logopath

--- src/components/PagesMenu.tsx
+++ src/components/PagesMenu.tsx
@@ -3,7 +3,7 @@ import "./PagesMenu.css";
 const PagesMenu = () => {
   return (
     <div className="group-pages">
-      <img src="/luisalogo.svg" className="logo" alt="Luisa Kuhn" />
+      <img src="luisalogo.svg" className="logo" alt="Luisa Kuhn" />
       <div className="pages">
         <a href="#projects">projects</a>
         <a href="#shots">shots</a>
